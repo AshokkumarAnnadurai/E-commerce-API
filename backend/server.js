93b0ff2284e55843c841c32559b98cf3c75f1a7a
Ashokkumar.a <ashokkumar.a@signatech.com>
2024-08-13 19:16:10 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const path = require("path");
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const mongodbUrl = "mongodb+srv://AshokkumarAnnadurai:Ashok@8220!@e-commerce.sethm.mongodb.net/?retryWrites=true&w=majority&appName=E-commerce"
+const mongodbUrl = process.env.mongodbUrl
 
 mongoose
     .connect(mongodbUrl, {
@@ -24,16 +24,6 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use("/api/products", productRoute);
-
-// Serve static assests if in production
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
 
 app.get('/api/health', (req, res) => {
     res.send({ status: 'healthy' });
